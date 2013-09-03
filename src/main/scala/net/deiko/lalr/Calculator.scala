c2e250a58b37945a64337539b9b86ed967dde9c0
YoEight <yo.eight@gmail.com>
2013-09-04 00:46:12 +0200

    Implement arithmetic expression evaluator

--- src/main/scala/net/deiko/lalr/Calculator.scala
+++ src/main/scala/net/deiko/lalr/Calculator.scala
@@ -1,15 +1,36 @@
 package net.deiko.lalr
 
+import scalaz.\/
+import scalaz.concurrent.Task
 import scalaz.stream._
 import Process._
 
 object Calculator {
+  val evaluator: Channel[Task, String \/ Mu[AstOp], String \/ Double] = {
+    def go(input: String \/ Mu[AstOp]): Task[String \/ Double] = {
+      def step(ast: Mu[AstOp]): Task[Double] =
+        Task.now {
+          ast.fold[Double] {
+            case Number(n) => n
+            case Mul(l, r) => l * r
+            case Div(l, r) => l / r
+            case Add(l, r) => l + r
+            case Sub(l, r) => l - r
+          }
+        }
+
+      input.traverse(step)
+    }
+
+    await(Task.now[(String \/ Mu[AstOp]) => Task[String \/ Double]](go))(emit).repeat
+  }
+
   def main(args: Array[String]) {
     import scalaz.std.string._
     import scalaz.syntax.foldable._
     import scalaz.concurrent.Task
 
     val source: Process[Task, Char] = emitAll("1 + 2 * 3")
-    println((source |> Lexer.lexer |> Parser.parser).collect.run)
+    println((source |> Lexer.lexer |> Parser.parser).through(evaluator).collect.run)
   }
 }
